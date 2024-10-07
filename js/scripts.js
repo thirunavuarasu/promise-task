12a0e79a3bf1955e90c05b6ecdfea8ed5f1cfe7f
thirunavuarasu <thirunavuarasu.m@gmail.com>
2024-10-07 22:37:10 +0530

    promise task

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Declaring the variable
+const button = document.querySelector(".button");
+//////////////////////////
+
+//// Window will refresh when button is clicked/////
+button.addEventListener("click", () => {
+    window.location.reload();
+   
+});
+/////////////////////////////////////////////////////
